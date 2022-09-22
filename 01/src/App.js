e4ca3e25c0dd87d5afa903a6dd2d58be3eedfc73
jid <idczak.jaroslaw@gmail.com>
2022-09-22 11:49:13 +0200

    exercise 01 template

---
+++ 01/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcom to The App!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
